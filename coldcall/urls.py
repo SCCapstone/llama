d1676d2a2031999316ce49e378b1e354e35a95f6
Cade Stocker <caderstocker@gmail.com>
2024-11-07 12:05:26 -0500

    made home page

--- coldcall/urls.py
+++ coldcall/urls.py
@@ -7,6 +7,7 @@ app_name = "coldcall"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("admin/", admin.site.urls),
+    path("home/", views.HomePageView.as_view(), name="home"),
     path("<int:course_id>/", views.CourseHomePageView.as_view(), name="course_home"),
     path("<int:course_id>/randomizer", views.StudentRandomizerView.as_view(), name="randomizer"),
     path("student/<int:student_id>", views.StudentMetricsView.as_view(), name="student_metrics"),
@@ -15,5 +16,4 @@ urlpatterns = [
     path("addstudents/addcourse", views.AddCourseView.as_view(), name="add_course"),
     path("editclasses/<int:course_id>/<int:student_id>/manual", views.EditStudentManualView.as_view(), name="edit_student_manual"),
     path("editclasses/<int:course_id>/<int:student_id>/csv", views.EditStudentCSVView.as_view(), name="edit_student_csv"),
-
 ]
