4abecb177b1458b301a8174e549bc7ba6ba69af9
Cade Stocker <caderstocker@gmail.com>
2024-11-07 11:51:42 -0500

    skeleton of views and added urls

--- coldcall/urls.py
+++ coldcall/urls.py
@@ -6,5 +6,14 @@ from . import views
 app_name = "coldcall"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("admin/", admin.site.urls)
+    path("admin/", admin.site.urls),
+    path("<int:course_id>/", views.CourseHomePageView.as_view(), name="course_home"),
+    path("<int:course_id>/randomizer", views.StudentRandomizerView.as_view(), name="randomizer"),
+    path("student/<int:student_id>", views.StudentMetricsView.as_view(), name="student_metrics"),
+    path("addstudents/manual", views.AddStudentManualView.as_view(), name="add_student_manual"),
+    path("addstudents/import", views.AddStudentImportView.as_view(), name="add_student_import"),
+    path("addstudents/addcourse", views.AddCourseView.as_view(), name="add_course"),
+    path("editclasses/<int:course_id>/<int:student_id>/manual", views.EditStudentManualView.as_view(), name="edit_student_manual"),
+    path("editclasses/<int:course_id>/<int:student_id>/csv", views.EditStudentCSVView.as_view(), name="edit_student_csv"),
+
 ]
--- coldcall/views.py
+++ coldcall/views.py
@@ -1,6 +1,43 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic import TemplateView, ListView
+from .models import Student, Class
 # Create your views here.
 
 class IndexView(generic.TemplateView):
     template_name = "coldcall/index.html"
+
+class HomePageView(ListView):
+    model = Student
+    template_name = "coldcall/home.html"
+    context_object_name = "students"
+
+class StudentRandomizerView(TemplateView):
+    template_name = "coldcall/randomizer.html"
+
+class CourseHomePageView(generic.DetailView):
+    model = Class
+    template_name = "coldcall/course_home.html"
+    context_object_name = "course"
+
+class StudentMetricsView(generic.DetailView):
+    model = Student
+    template_name = "coldcall/student_metrics.html"
+    context_object_name = "student"
+
+class AddCourseView(TemplateView):
+    template_name = "coldcall/add_course.html"
+
+class AddStudentImportView(TemplateView):
+    template_name = "coldcall/add_student_import.html"
+
+class AddStudentManualView(TemplateView):
+    template_name = "coldcall/add_student_manual.html"
+
+class EditStudentManualView(generic.DetailView):
+    model = Student
+    template_name = "coldcall/edit_student_manual.html"
+
+class EditStudentCSVView(generic.DetailView):
+    model = Student
+    template_name = "coldcall/edit_student_csv.html"
