649db36bc10954d0fdbad87b6d7b95ec09d35039
Cade Stocker <caderstocker@gmail.com>
2025-02-17 10:50:28 -0500

    style changes to login and register

--- coldcall/forms.py
+++ coldcall/forms.py
@@ -1,9 +1,17 @@
 
-from django.forms import ModelForm, CharField, PasswordInput
+from django.forms import ModelForm, CharField, PasswordInput, TextInput, EmailInput
 from django.contrib.auth import get_user_model
 
 class RegisterUserForm(ModelForm):
-    password = CharField(widget=PasswordInput())
+    password = CharField(widget=PasswordInput(attrs={'class': 'form-control'}))
     class Meta:
         model = get_user_model()
         fields = ["username", "first_name", "last_name", "email", "password"]
+        widgets = {
+            # trying to make the text bold in register (to look like login)
+            'username': TextInput(attrs={'class': 'form-control'}),
+            'first_name': TextInput(attrs={'class': 'form-control'}),
+            'last_name': TextInput(attrs={'class': 'form-control'}),
+            'email': EmailInput(attrs={'class': 'form-control'}),
+            'password': PasswordInput(attrs={'class': 'form-control'}),
+        }
